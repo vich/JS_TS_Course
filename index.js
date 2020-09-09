5c8caca331df4b8d1aee69106a3497bb83316ac7
Slava Korenblit <slavak@codevalue.com>
2020-09-09 19:27:23 +0300

    hw1

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function IsDiv3(number){
+    return (number % 3) == 0;
+}
+
+function IsDiv5(number){
+    return (number % 5) == 0;
+}
+
+function hw1(start, end)
+{
+    for (let i = start; i < end; i++) {
+
+        isDiv3 = IsDiv3(i);
+        isDiv5 = IsDiv5(i);
+
+        if(isDiv3 && isDiv5)
+            console.log(`${i} FizzBuzz`);
+        else{
+            if(isDiv3)
+                console.log(`${i} Fizz`);
+            if(isDiv5)
+            console.log(`${i} Buzz`);
+        }
+    }
+}
+
+hw1(1,100);
