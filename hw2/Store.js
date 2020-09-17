28f5ed10b82183fa0bc8779c74f0c0c0dd14cf2e
Slava Korenblit <slavak@codevalue.com>
2020-09-17 17:21:23 +0300

    const

--- hw2/Store.js
+++ hw2/Store.js
@@ -11,18 +11,16 @@ export class Store {
 
 
     addOrder(customerId, ...productsIds){
-        let validFlag = this.validateOrderParams(customerId, productsIds);
+        const validFlag = this.validateOrderParams(customerId, productsIds);
 
         if(validFlag){
             productsIds.forEach((id) => {
-                let product = this.products.find(p=>p.id === productId);
+                let product = this.products.find(p=>p.id === id);
                 --product.itemsInStock;
             });
             
-            let newOrder = new Order(customerId, productsIds);
-            this.printOrders();
+            const newOrder = new Order(customerId, productsIds);
             this.orders.push(newOrder);
-            this.printOrders();
         }
     }
 
@@ -30,14 +28,15 @@ export class Store {
     validateOrderParams(customerId, productsIds) {
         let validFlag = true;
         //validate customer
-        if (!this.customers.find(c => c.id === customerId)) {
+        const customer = this.customers.find(c => c.id === customerId);
+        if (!customer) {
             console.log(`Fails, invalid customer ${customerId}`);
             validFlag = false;
         }
 
         //validate productsIds
         productsIds.forEach((id) => {
-            let product = this.products.find(p => p.id === productId);
+            const product = this.products.find(p => p.id === id);
             if (product) {
                 if (!(product.itemsInStock > 0)) {
                     console.log(`Fails, missing items of productId ${id}`);
@@ -59,19 +58,19 @@ export class Store {
         console.log(`orders contains ${ this.orders.length} rows`);
         for (let i = 0; i < this.orders.length; i++){
             let order = this.orders[i];
-            console.log(`Order (${i}) - ${order}`);
+            console.log(`Order (${i+1}) - ${order}`);
         }
     }
 
 
     save(){
-        let jsonData = JSON.stringify(this);
+        const jsonData = JSON.stringify(this);
         return jsonData;
     }
 
 
     load(state){
-        let store = JSON.parse(state)
+        const store = JSON.parse(state)
 
         this.products = store.products;
         this.customers = store.customers;
@@ -81,7 +80,7 @@ export class Store {
 
     notify(){
         setInterval(()=> {
-            let productsOutOfStock = this.products.filter(product => !product.itemsInStock);
+            const productsOutOfStock = this.products.filter(product => !product.itemsInStock);
             console.log(`Out of stock products: ${JSON.stringify(productsOutOfStock)}`);
         }, NOTIFY_INTERVAL);
     }
