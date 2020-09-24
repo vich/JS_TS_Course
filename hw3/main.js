09767e41798572c4cea73d63c1c78499d19d6d99
Slava Korenblit <slavak@codevalue.com>
2020-09-24 14:33:00 +0300

    hw3

---
+++ hw3/main.js
@@ -0,0 +1,19 @@
+function myAsyncGreater10Function(number) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function(){
+            if(number > 10)
+                resolve('Yes!'); // Yay! Everything went well!
+            else
+                reject('No!');
+            }, 500);
+    });
+  }
+
+  let five = myAsyncFunction(5)
+            .then((msg) => console.log('5>10?  ' + msg))
+            .catch((msg) => console.log('5>10?  ' + msg));
+
+            
+  let twenty = myAsyncFunction(20) 
+            .then((msg) => console.log('20>10?  ' + msg))
+            .catch((msg) => console.log('20>10?  ' + msg));
